1fdba8f22b59693adc2b8db90364008ee0ddb31a
Paul Pan <panjunhong05@gmail.com>
2022-10-28 03:29:37 +0100

    Implemented Lateinit and Delegate properties 1 practice

--- src/main/kotlin/dev/sebastiano/workshop/01. Lateinit.kt
+++ src/main/kotlin/dev/sebastiano/workshop/01. Lateinit.kt
@@ -5,8 +5,16 @@ internal class LateInit {
     private lateinit var _fruit: String
 
     var fruit: String?
-        get() = TODO()
-        set(value) = TODO()
+        get() {
+            return if (::_fruit.isInitialized) {
+                _fruit
+            } else {
+                null
+            }
+        }
+        set(value) {
+            _fruit = value ?: throw IllegalArgumentException()
+        }
 
     fun performSomeWork(): String = "Here's something for ya: $_fruit"
 }
--- src/main/kotlin/dev/sebastiano/workshop/02. Delegate properties 1.kt
+++ src/main/kotlin/dev/sebastiano/workshop/02. Delegate properties 1.kt
@@ -4,5 +4,12 @@ import dev.sebastiano.workshop.util.ValueProvider
 
 class DelegateProperties1(private val valueProvider: ValueProvider<String>) {
 
-    val fruit: String = TODO()
+    private lateinit var value: String
+    val fruit: String
+        get() {
+            if (!::value.isInitialized) {
+                value = valueProvider.provideValue()
+            }
+            return value
+        }
 }
